0906136621f64f9c06e4917cd1db2aca38cdac4e
Khoa Pham <onmyway133@gmail.com>
2020-05-07 07:31:40 +0200

    Site updated: 2020-05-07 07:31:37

---
+++ pastepal/precache-manifest.2dfee35ec65667febea478c87397fc62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5be2962f4df783e093fad7f14bb5bf0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59d4b6e5cabc053ae243",
+    "url": "./static/css/main.9c1bbfc8.chunk.css"
+  },
+  {
+    "revision": "62954656db8e2713920e",
+    "url": "./static/js/2.24ca8c2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.24ca8c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d4b6e5cabc053ae243",
+    "url": "./static/js/main.9fccb7e1.chunk.js"
+  },
+  {
+    "revision": "608c487eea2ebcd8b686",
+    "url": "./static/js/runtime-main.974c5c28.js"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/1.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/2.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/3.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/4.863e9cf9.png"
+  }
+]);
---
+++ pastepal/precache-manifest.f383ed59f3e1e5740695aa971e0e0517.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028e7126117d5f139bdb4ceef17a1d8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6bab5b29696b31029d5",
+    "url": "./static/css/main.c80ae401.chunk.css"
+  },
+  {
+    "revision": "62954656db8e2713920e",
+    "url": "./static/js/2.24ca8c2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.24ca8c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bab5b29696b31029d5",
+    "url": "./static/js/main.b9dd2d79.chunk.js"
+  },
+  {
+    "revision": "608c487eea2ebcd8b686",
+    "url": "./static/js/runtime-main.974c5c28.js"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/1.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/2.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/3.863e9cf9.png"
+  },
+  {
+    "revision": "863e9cf9748c6d77fc2c1ec0de0d4f6e",
+    "url": "./static/media/4.863e9cf9.png"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "./static/media/download iOS.0f3514a4.svg"
+  },
+  {
+    "revision": "b36bcf724bc71f295292ea1fbf066aa1",
+    "url": "./static/media/download macOS.b36bcf72.svg"
+  },
+  {
+    "revision": "8d7bcdcbb38444c429ba2591999def7d",
+    "url": "./static/media/download tvOS.8d7bcdcb.svg"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "./static/media/download watchOS.0f3514a4.svg"
+  }
+]);
